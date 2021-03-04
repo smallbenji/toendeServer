c6f4cdce7921a90bf73db066fcae2c399fa574ea
smallbenji <benjaminkfalch@gmail.com>
2021-03-04 21:16:54 +0100

    lets go

--- index.js
+++ index.js
@@ -8,12 +8,15 @@ const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
 
 const server = express()
-    .use((req, res) => res.sendFile(INDEX, {
-        root: __dirname
-    }))
+    .use((req, res) => res.sendFile(INDEX, {root: __dirname}))
     .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const wss = new Server({ server });
+
+wss.on('connection', (ws) => {
+    console.log('Client connected');
+    ws.on('close', () => console.log('Client disconnected'));
+});
 
 var toende = {
     "hp": 0
